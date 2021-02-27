a2817c37befd9bdfe5499b73a69c952413c30940
Ivannikov Roman <roman.y.ivannikov@gmail.com>
2021-02-27 11:27:08 +0700

    build: lesson 22 complicated

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ class Todo {
     this.form = document.querySelector(form);
     this.input = document.querySelector(input);
     this.todoList = document.querySelector(todoList);
-    this.todoCompleted = document.querySelector(todoCompleted);
+    this.todoCompleted = document.querySelector(todoCompleted),
+    this.todoContainer = this.todoList.parentNode,
     this.todoData = new Map(JSON.parse(localStorage.getItem('toDoList')));
   }
 
@@ -28,6 +29,7 @@ class Todo {
     li.insertAdjacentHTML('beforeend', `
       <span class="text-todo">${todo.value}</span>
       <div class="todo-buttons">
+        <button class="todo-edit"></button>
         <button class="todo-remove"></button>
         <button class="todo-complete"></button>
       </div>
@@ -61,7 +63,7 @@ class Todo {
   }
 
   deleteItem(item) {
-    if (this.todoData.delete(item.key)) {
+    const fading = (callback) => {
       let opacity = 1, idRequest;
       const opacityItem = () => {
         idRequest = requestAnimationFrame(opacityItem);
@@ -70,59 +72,93 @@ class Todo {
         opacity -= 0.065;
   
         if (opacity < 0) {
+          item.style.opacity = 0;
           cancelAnimationFrame(idRequest);
+          callback.call(this);
           return;
         }
       };
       opacityItem();
-
-      this.render();
-    }
-
-    // if (this.todoData.delete(key)) {
-    //   this.render();
-    // }
-  }
-
-  animationDeleteItem(item) {
-    let opacity = 1, idRequest;
-
-    const opacityItem = () => {
-      opacity -= 0.01;
-      if (opacity < 0) {
-        item.style.opacity = 0;
-        cancelAnimationFrame(idRequest);
-        return;
-      }
-      item.style.opacity = opacity;
-      idRequest = requestAnimationFrame(opacityItem);
     };
-    opacityItem();
+    this.todoData.delete(item.key);
+    fading(this.render);
   }
 
-  completeItem(key) {
-    if (this.todoData.has(key)) {
-      const item = this.todoData.get(key);
-      item.completed = !item.completed;
-      this.todoData.set(key, item);
-      this.render();
+  completeItem(item) {
+
+    const itemTodo = this.todoData.get(item.key);
+
+    let distance = this.todoCompleted.offsetTop + (this.todoCompleted.offsetHeight / 2) - item.offsetTop,
+      moveDown = item.offsetTop < this.todoCompleted.offsetTop;
+  
+    if (itemTodo.completed) {
+      // Если дело переносится в незавершенные дела
+      distance = item.offsetTop - (this.todoList.offsetTop + (this.todoList.offsetHeight / 2));
     }
+
+    const move = (callback) => {
+      let idRequest;
+      let currentPos = 0;
+      const deltaPos = distance / 10;
+      const moveItem = () => {
+        idRequest = requestAnimationFrame(moveItem);
+
+        item.style.top = `${currentPos}px`;
+        currentPos = moveDown ? currentPos + deltaPos : currentPos - deltaPos;
+        if (Math.abs(currentPos) > distance) {
+          cancelAnimationFrame(idRequest);
+          callback.call(this);
+          return;
+        }
+      };
+      moveItem();
+
+    };
+
+    move(this.render);
+    itemTodo.completed = !itemTodo.completed;
+    this.todoData.set(item.key, itemTodo);
+  }
+
+  editItem(item) {
+
+    const liItems = document.querySelectorAll('.todo-container li');
+    liItems.forEach((li) => {
+      const textLi = li.querySelector('span');
+      if (!li.getAttribute('contenteditable') && li.key === item.key) {
+        li.style.border = '1px solid black';
+        li.style.backgroundColor = '#f3f2f2';
+        li.querySelector('button.todo-edit').classList.add('todo-editactive');
+        li.setAttribute('contenteditable', true);
+      } else if (li.getAttribute('contenteditable')) {
+        li.removeAttribute('style');
+        li.removeAttribute('contenteditable');
+        li.querySelector('button.todo-edit').classList.remove('todo-editactive');
+
+        const itemTodo = this.todoData.get(item.key);
+        itemTodo.value = li.querySelector('span').textContent.trim();
+        this.todoData.set(item.key, itemTodo);
+        this.render();
+      }
+    });
+
   }
 
   handler() {
-    this.todoList.parentNode.addEventListener('click', (event) => {
+    this.todoContainer.addEventListener('click', (event) => {
       const target = event.target;
 
       if (target.tagName === 'BUTTON') {
         const li = target.closest('.todo-item');
-        // const key = target.closest('.todo-item').key;
         if (li.key) {
           if (target.classList.contains('todo-remove')) {
-            // this.animationDeleteItem(li);
             this.deleteItem(li);
           }
           if (target.classList.contains('todo-complete')) {
-            // this.completeItem(key);
+            this.completeItem(li);
+          }
+          if (target.classList.contains('todo-edit')) {
+            this.editItem(li);
           }
         }
       }
